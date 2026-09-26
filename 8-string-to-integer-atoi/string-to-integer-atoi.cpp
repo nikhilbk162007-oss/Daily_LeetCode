98a8c4b593a4b8ba2ae5d8884c4c13f73e46c78e
nikhilbk162007-oss <nikhilbk162007@gmail.com>
2026-09-26 11:49:59 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (80.80%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.cpp
+++ 8-string-to-integer-atoi/string-to-integer-atoi.cpp
@@ -1,39 +1,35 @@
 class Solution {
 public:
     int myAtoi(string s) {
-        int n = s.size();
+        int n = s.length();
         int i = 0;
 
-        // 1. Skip leading spaces
-        while (i < n && s[i] == ' ') {
+        while(i<n && s[i] == ' '){
             i++;
         }
 
-        // 2. Check sign
         int sign = 1;
-
-        if (i < n && (s[i] == '+' || s[i] == '-')) {
-            if (s[i] == '-') {
+        if(i<n && (s[i] == '+' || s[i] == '-')){
+            if(s[i]=='-'){
                 sign = -1;
             }
             i++;
         }
 
-        // 3. Convert digits
         long long num = 0;
-
-        while (i < n && isdigit(s[i])) {
+        while(i<n && isdigit(s[i])){
             int digit = s[i] - '0';
 
-            // 4. Check overflow before adding digit
-            if (num > (INT_MAX - digit) / 10) {
-                if (sign == 1)
+            if(num > (INT_MAX - digit)/10){
+                if(sign == 1){
                     return INT_MAX;
-                else
+                }
+                else{
                     return INT_MIN;
+                }
             }
 
-            num = num * 10 + digit;
+            num = num*10 + digit;
             i++;
         }
 
